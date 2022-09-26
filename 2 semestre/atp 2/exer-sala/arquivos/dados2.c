8357c5be6e5fd06b58240c2786a1319e30090ab6
William Martins <72496595+WilliamKotoco@users.noreply.github.com>
2022-09-26 17:42:32 -0300

    update dnv

--- 2 semestre/atp 2/exer-sala/arquivos/dados2.c
+++ 2 semestre/atp 2/exer-sala/arquivos/dados2.c
@@ -5,6 +5,10 @@ int main(){
     FILE *sourcefile, *destinationfile;
 
     sourcefile = fopen("datas.dat", "r");
+	if(sourcefile == NULL)
+	{
+		printf("Error: Couldn't open source file");
+	}
     destinationfile = fopen("esquisito.dat", "w");
 
     while(!(feof(sourcefile))){
