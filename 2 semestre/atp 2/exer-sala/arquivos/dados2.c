04a9e874399c65467247fad96678545b89a49729
William Martins <72496595+WilliamKotoco@users.noreply.github.com>
2022-09-26 17:38:56 -0300

    update datos.c

--- 2 semestre/atp 2/exer-sala/arquivos/dados2.c
+++ 2 semestre/atp 2/exer-sala/arquivos/dados2.c
@@ -1,33 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-	int i = 0;
-	char number[200];
-	char letra = 'A';
-	FILE *sourcefile, *destinationfile;
-	sourcefile = fopen("datas.dat", "r");
-	if(sourcefile== NULL)
-	{
-		printf("Arquivo nao existe \n");
-	}
-	destinationfile = fopen("esquisito.dat", "w");
-	while(!feof(sourcefile))
-	{
-		fscanf(sourcefile, "%s", &number);
-		if(number[i] == ' ')
-		{
-			fprintf(destinationfile, "%d", letra);
-			letra = (letra+1- 'A')%26 + 'A';
-		}
-		else
-		{
-			fprintf(destinationfile, "%s", number);
-		}
-		i++;
-	}
-	fclose(sourcefile);
-	fclose(destinationfile);
-	return 0;
+#include <stdio.h>
 
+int main(){
+    int letra=0, num;
+    FILE *sourcefile, *destinationfile;
+
+    sourcefile = fopen("datas.dat", "r");
+    destinationfile = fopen("esquisito.dat", "w");
+
+    while(!(feof(sourcefile))){
+        fscanf(sourcefile, "%d ", &num);
+        fprintf(destinationfile, "%d%c", num, ((letra % 26) + 65));
+        letra++;
+    }
+    fclose(sourcefile);
+    fclose(destinationfile);
 }
