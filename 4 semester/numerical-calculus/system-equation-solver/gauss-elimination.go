65b34f8ba27ed92f106570f12ecc24e12a51ed3f
William Martins <williamdabruzzmartins76136@gmail.com>
2023-08-13 17:15:57 -0300

    end the gauss elimination with partial pivot
    The next step is creating an algorithm to solve using the LU factoration

--- 4 semester/numerical-calculus/system-equation-solver/gauss-elimination.go
+++ 4 semester/numerical-calculus/system-equation-solver/gauss-elimination.go
@@ -17,19 +17,39 @@ import (
 
 func main() {
 	matrix := read_matrix()
+	fmt.Println("Matriz inicial: ")
 	fmt.Println(matrix)
+	fmt.Println(" ")
 
 	/// the first step is to transform it in a triangular matrix. We'll do it with the gaussian elimination with a partial pivot
 
-	for step := 0; step < len(matrix); step++ {
+	for step := 0; step < len(matrix)-1; step++ {
 		pivot := get_pivot(step, matrix)
 
 		multipliers := create_multipliers(step, matrix, pivot)
 
-		update_matrix(matrix, multipliers)
+		update_matrix(matrix, multipliers, step)
 
-		fmt.Println(matrix)
+		//fmt.Println(matrix)
 	}
+	fmt.Println("Final matrix:")
+	fmt.Println(matrix)
+	/// now use the algorithm to solve the system of a triangular matrix
+	matrix_last_index := len(matrix) - 1
+	solutions := make([]float64, len(matrix))
+
+	/// the answers are in the last_index + 1.
+	solutions[matrix_last_index] = matrix[matrix_last_index][matrix_last_index+1] / matrix[matrix_last_index][matrix_last_index]
+	for i := matrix_last_index - 1; i >= 0; i-- {
+		var sum float64 = 0
+		for j := i + 1; j < len(matrix); j++ {
+			sum += matrix[i][j] * solutions[j]
+		}
+		solutions[i] = (matrix[i][matrix_last_index+1] - sum) / matrix[i][i]
+
+	}
+	fmt.Println("Solutions: ")
+	fmt.Println(solutions)
 }
 
 func read_matrix() [][]float64 {
@@ -69,10 +89,10 @@ func read_matrix() [][]float64 {
 func get_pivot(step int, matrix [][]float64) float64 {
 	var biggest float64 = 0
 
-	/// the number of lines is the number of columns  - 1
+	/// the number of lines are the number of columns  - 1
 	size := len(matrix[0]) - 1
 	var biggest_line int
-	for i := 0; i < size; i++ {
+	for i := step; i < size; i++ {
 		if matrix[i][step] > biggest {
 			biggest = matrix[i][step]
 			biggest_line = i
@@ -90,11 +110,11 @@ Create the multipliers according to the step and pivot. Returns a slice of them
 */
 func create_multipliers(step int, matrix [][]float64, pivot float64) []float64 {
 
-	multipliers := make([]float64, len(matrix)-step)
+	multipliers := make([]float64, len(matrix))
 
-	/// multipliers always made for the lines above the step
+	/// multipliers always made for the lines below the step
 	for i := step + 1; i < len(matrix); i++ {
-		multipliers[i-step-1] = matrix[i][step] / pivot
+		multipliers[i] = matrix[i][step] / pivot
 	}
 
 	return multipliers
@@ -102,6 +122,14 @@ func create_multipliers(step int, matrix [][]float64, pivot float64) []float64 {
 }
 
 // / do the updates based on the multipliers, adding and permutating lines
-func update_matrix(matrix [][]float64, multipliers []float64) {
+func update_matrix(matrix [][]float64, multipliers []float64, step int) {
 
+	for i := step + 1; i < len(matrix); i++ {
+
+		/// multiplies each element of the line.
+		for j := 0; j < len(matrix[i]); j++ {
+			matrix[i][j] -= multipliers[i] * matrix[step][j]
+		}
+
+	}
 }
