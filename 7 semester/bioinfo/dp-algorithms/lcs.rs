057b0352d9fd23da5aac6ff1b95b3ae1b6aafe01
WilliamKotoco <williamdabruzzmartins76136@gmail.com>
2025-03-04 11:46:19 -0300

    dp

--- 7 semester/bioinfo/dp-algorithms/lcs.rs
+++ 7 semester/bioinfo/dp-algorithms/lcs.rs
@@ -5,7 +5,7 @@ fn create_dp_matrix(n: usize, m: usize) -> Vec<Vec<i32>> {
     vec![vec![0; m + 1]; n + 1]
 }
 
-fn lcs(s1: Vec<char>, s2: Vec<char>) -> i32 {
+fn lcs(s1: Vec<char>, s2: Vec<char>) -> (i32, String) {
     let n = s1.len();
     let m = s2.len();
     let mut matrix = create_dp_matrix(n, m);
@@ -19,8 +19,28 @@ fn lcs(s1: Vec<char>, s2: Vec<char>) -> i32 {
             }
         }
     }
+    
+    let mut lcs_string = String::new();
+    let mut i = n; 
+    let mut j = m;
 
-    matrix[n][m]
+    // backtrac to rebuild the string 
+    while i > 0 && j > 0 {
+        if s1[i-1] == s2[j-1]{
+            lcs_string.push(s1[i-1]);
+            i -= 1;
+            j -= 1;
+        }
+        else if matrix[i-1][j] > matrix[i][j-1] {
+            i -=1;
+        }
+        else{
+            j -=1;
+        }
+    }
+
+    lcs_string = lcs_string.chars().rev().collect();
+    (matrix[n][m], lcs_string)
 }
 
 fn main() {
@@ -44,7 +64,8 @@ fn main() {
     let s1: Vec<char> = s1.chars().collect();
     let s2: Vec<char> = s2.chars().collect();
 
-    let lcs = lcs(s1, s2);
+    let (lcs, lcs_str) = lcs(s1, s2);
 
-    println!("{lcs}")
+    println!(" LCS Length: {lcs}");
+    println!("LCS: {lcs_str}");
 }
